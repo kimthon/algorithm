42ba11041968bac8d09b687a8197f4c22ce18d76
kimthon <xoghks9793@gmail.com>
2020-12-24 13:48:17 +0900

    uncompleted 60058.cpp

--- Programmers/60057.cpp
+++ Programmers/60057.cpp
@@ -10,29 +10,34 @@ int solution(string s) {
 
 	for(int i = 1; i <= max; ++i) {
 		int size = s.length();
-		string tmp = "";
-		string str1 = s.substr(0, i);
 		int count = 1;
+		int num = size % i;
+		string str1 = s.substr(0, i);
+
 		for(int j = i; j <= size; j += i) {
 			string str2 = s.substr(j, i);
 			if(str1.compare(str2) == 0) {
 				++count;
 			} else {
 				if(count != 1) {
-					tmp += to_string(count);
+					do {
+						++num;
+					}while((count = count / 10) > 0);
 					count = 1;
 				}
-				tmp += str1;
+				num += i;
 				str1 = str2;
 			}
 		}
 
-		if(count != 1) tmp += to_string(count) + str1;
-
-		int num = tmp.length() + size%i;
+		if(count != 1) {
+			do {
+				++num;
+			}while((count = count / 10) > 0);
+			num += i;
+		}
 		if(num < answer) answer = num;
 	}
-
 	return answer;
 }
 
---
+++ Programmers/60058.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+string solution(string p) {
+	string answer = "";
+	return answer;
+}
+
+int main() {
+	string s;
+	cin >> s;
+
+	cout << solution(s) << endl;
+
+	return 0;
+}
